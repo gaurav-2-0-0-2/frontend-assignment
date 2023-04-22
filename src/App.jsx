4193a75dba0081463e03325de98953d86487fb81
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-04-22 23:34:41 +0530

    added some comment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Input from './components/Input'  
-import DynamicForm from './components/Form'
+// import DynamicForm from './components/Form'
 
 
 
@@ -15,7 +15,7 @@ function App() {
 
 
       <Input />
-      <DynamicForm/>  
+      {/* <DynamicForm/>   */}
 
 
     </div>
