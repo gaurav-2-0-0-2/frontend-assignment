b4b299ca4a72a2ebbea5c0504a808e4cce8a8f08
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-04-25 19:17:28 +0530

    implement form submission

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,42 +1,14 @@
-// import { RJSFSchema } from '@rjsf/utils';
-// import validator from '@rjsf/validator-ajv8';
-// import Form from '@rjsf/core';
 import { useEffect, useState } from 'react';
-import { Dropdown } from 'antd';
-// import { inputData } from '../data/data';
-// import schema from '../data/fakeData.js';
+import { Switch } from 'antd';
+
 
 export default function Input() {
 
-
-    // export default function Input({ jsonObject, onJsonChange }) {
-    //     const [jsonData, setJsonData] = useState('');
-
-    //     useEffect(() => {
-    //         // setJsonData(JSON.stringify(jsonObject, null, 2));
-    //         try {
-    //             const parsedData = JSON.parse(jsonObject);
-    //             onJsonChange(parsedData);
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     }, [jsonObject]);
-
-    //     const handleInputChange = (event) => { 
-    //         setJsonData(event.target.value);
-    //     };
-
-    //     return (
-    //         <div>
-    //             <label>
-    //                 <textarea className='bg-gray-100 p-4' cols={50} rows={15} value={jsonData} onChange={handleInputChange} />
-    //             </label>
-    //         </div>
-    //     );
-    // }
-
     const [jsonData, setJsonData] = useState('');
     const [jsonObject, setJsonObject] = useState([]);
+    const [toggle, setToggle] = useState(false);
+    const [toggleSS, setToggleSS] = useState(false);
+    const [formData , setFormData] = useState('');
 
 
     useEffect(() => {
@@ -56,69 +28,163 @@ export default function Input() {
         // setJsonData(value);
     }
 
+
+    const onChange = (checked) => {
+        console.log(`switch to ${checked}`);
+        setToggle(checked);
+    };
+
+    const onChangeAnother = (checked) => {
+        console.log(`switch to ${checked}`);
+        setToggleSS(checked);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // alert(JSON.stringify(jsonObject));
+    }
+
     return (
         <div className='grid grid-cols-2'>
             <div>
                 <textarea className='bg-gray-100 outline-none p-4' onChange={handleChange} value={jsonData} name="" cols="50" rows="24"></textarea>
             </div>
 
-            {/* {jsonObject && <pre>{JSON.stringify(jsonObject, null, 2)}</pre>} */}
             <div>
+
                 {jsonObject.map((item, index) => {
                     return (
-                        <div key={index} className=' bg-blue-100 p-5'>
-                            {index === 0 ? (
-                                <div className='flex flex-row gap-5 justify-between'>
-                                    <h1>{item.label}</h1>
-                                    <input className='outline-none px-2' placeholder={item.placeholder} type="text" />
-                                </div>
-                            ) : index === 1 ? (
-                                <div>
-                                    <h1>{item.label}</h1>
-                                    <div className='flex gap-5 justify-between'>
-                                        <button className=' bg-blue-200 p-5'>{item.subParameters[0].validate.options[0].label}</button>
-                                        <button className=' bg-blue-200 p-5'>{item.subParameters[0].validate.options[1].label}</button>
-                                    </div>
-                                    {/* Slices sub section  */}
-                                    <div className='flex justify-between mt-5'>
-                                        <h1>{item.subParameters[1].subParameters[0].label}</h1>
-                                        <button className='bg-blue-200'>dropdown</button>
-                                    </div>
-                                </div>
 
-                            ) : index === 2 ? (
-                                <div>
-                                    {/* Toppings Section  */}
-                                    <div >
+
+                        <form onSubmit={handleSubmit}>
+                            <div key={index} className=' bg-blue-100 p-10'>
+                                <h1 className='text-2xl'>New Pizza</h1>
+                                {index === 0 ? (
+                                    <div className='flex flex-row gap-5 justify-between mt-10'>
                                         <h1>{item.label}</h1>
-                                        {/* Sauce sub-section  */}
+                                        <input className='outline-none px-2' name='pizza_name' onChange={(e)=>setFormData(e.target.value)} placeholder={item.placeholder} type="text" />
+                                    </div>
+                                ) : index === 1 ? (
+                                    <div>
+                                        <h1>{item.label}</h1>
+                                        <div className='flex gap-5 justify-between'>
+                                            <button className=' bg-blue-200 p-5'>{item.subParameters[0].validate.options[0].label}</button>
+                                            <button className=' bg-blue-200 p-5'>{item.subParameters[0].validate.options[1].label}</button>
+                                        </div>
+                                        {/* Slices sub section  */}
                                         <div className='flex justify-between mt-5'>
-                                            <h1>{item.subParameters[0].label}</h1>
-                                            <button className='bg-blue-200'>dropdown</button>
+                                            <h1>{item.subParameters[1].subParameters[0].label}</h1>
+                                            {/* <button className='bg-blue-200'>dropdown</button> */}
+                                            <select>
+                                                {item.subParameters[1].subParameters[0].validate.options.map((slice) => (
+                                                    <option value={slice.value}>{slice.value}</option>
+                                                ))}
+                                            </select>
+
+
+
                                         </div>
-                                        {/* Main Topping sub-section  */}
-                                        <div className='flex justify-between mt-5'>
-                                            <h1>{item.subParameters[1].label}</h1>
-                                            <button className='bg-blue-200'>dropdown</button>
+                                    </div>
+
+                                ) : index === 2 ? (
+                                    <div>
+                                        {/* Toppings Section  */}
+                                        <div >
+                                            <h1>{item.label}</h1>
+                                            {/* Sauce sub-section  */}
+                                            <div className='flex justify-between mt-5'>
+                                                <h1>{item.subParameters[0].label}</h1>
+                                                {/* DropDown  */}
+                                                <select>
+                                                    {item.subParameters[0].validate.options.map((slice) => (
+                                                        <option value={slice.value}>{slice.value}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
+                                            {/* Main Topping sub-section  */}
+                                            <div className='flex justify-between mt-5'>
+                                                <h1>{item.subParameters[1].label}</h1>
+                                                {/* DropDown  */}
+                                                <select>
+                                                    {item.subParameters[1].validate.options.map((slice) => (
+                                                        <option value={slice.value}>{slice.value}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
+
+                                            {/*Include seasoning checkbox*/}
+
+                                            <div className='flex'>
+                                                <input type="checkbox" name='' id="" />
+                                                <h1>{item.subParameters[2].label}</h1>
+                                            </div>
+
+                                            <div className='mt-5'>
+                                                {toggle ? (
+                                                    <div className='flex justify-between'>
+                                                        <h1>{item.subParameters[3].label}</h1>
+                                                        <select>
+                                                            {item.subParameters[3].validate.options.map((slice) => (
+                                                                <option value={slice.value}>{slice.value}</option>
+                                                            ))}
+                                                        </select>
+                                                    </div>
+
+                                                ) : null}
+                                                <div className='flex gap-4'>
+                                                    {toggle ? (
+                                                        <h1>Hide advanced field</h1>
+                                                    ) : (
+                                                        <h1>Show advanced field</h1>
+                                                    )}
+                                                    <Switch size='small' onChange={onChange} />
+                                                </div>
+
+
+                                            </div>
+
                                         </div>
-
-                                        {/*Include seasoning checkbox*/}
-
-                                        <div className='flex'>
-                                            <input type="checkbox" name='' id="" />
-                                            <h1>{item.subParameters[2].label}</h1>
-                                        </div>
-
-                                        
-
 
                                     </div>
 
-                                </div>
-                            ) : null}
-                        </div>
+                                ) : index === 3 ? (
+
+                                    <div>
+                                        {toggleSS ? (
+                                            <div className='flex justify-between'>
+                                                <h1>{item.label}</h1>
+                                                <select>
+                                                    {item.validate.options.map((slice) => (
+                                                        <option value={slice.value}>{slice.value}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
+
+                                        ) : null}
+                                        <div className='flex gap-4 justify-between'>
+                                            <div className='flex gap-4'>
+                                                {toggleSS ? (
+                                                    <h1>Hide advanced field</h1>
+                                                ) : (
+                                                    <h1>Show advanced field</h1>
+                                                )}
+                                                <Switch size='small' onChange={onChangeAnother} />
+                                            </div>
+
+                                            <div className='flex gap-5'>
+                                                <button className=' bg-transparent border-red-300'>Cancel</button>
+                                                <button type='submit' className=' bg-gray-400 text-white'>Submit</button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : null}
+                            </div>
+                        </form>
                     )
+
                 })}
+
             </div>
 
         </div>
