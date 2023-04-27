1487f03c4ac33fe8a73c27fc73e6aef8357eaff3
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-04-27 23:31:00 +0530

    improved dropdown design

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -50,140 +50,146 @@ export default function Input() {
                 <textarea className='bg-gray-100 outline-none p-4' onChange={handleChange} value={jsonData} name="" cols="50" rows="24"></textarea>
             </div>
 
-            <div>
-
+            <div className='bg-white p-10 border-solid border-4 border-blue-400 rounded-xl'>
+                <h1 className='text-2xl font-bold'>New Pizza</h1>
+                 <hr className='my-4 border-2 border-gray-200'/>
                 {jsonObject.map((item, index) => {
                     return (
 
 
                         <form onSubmit={handleSubmit}>
 
-                            <div key={index} className='p-10 bg-blue-500 rounded-xl my-4'>
 
-                                    <h1 className='text-2xl'>New Pizza</h1>
-                                    {index === 0 ? (
-                                        <div className='flex flex-row gap-5 justify-between mt-10'>
-                                            <h1>{item.label}</h1>
-                                            <input className='outline-none px-2 rounded-md w-[30rem] h-[3rem]' name='pizza_name' onChange={(e) => setFormData(e.target.value)} placeholder={item.placeholder} type="text" />
+                            <div key={index} className='p-5 bg-gray-100 rounded-xl my-4'>
+
+                                {/* <h1 className='text-2xl'>New Pizza</h1> */}
+                                {index === 0 ? (
+                                    <div className='flex flex-row gap-5 justify-between mt-10'>
+                                        <h1 className='font-bold'>{item.label}</h1>
+                                        <input className='outline-none bg-blue-100 px-2 rounded-md w-[30rem] h-[3rem]' name='pizza_name' onChange={(e) => setFormData(e.target.value)} placeholder={item.placeholder} type="text" />
+                                    </div>
+                                ) : index === 1 ? (
+                                    <div>
+                                        <h1 className='font-bold'>{item.label}</h1>
+                                        <hr className='my-4 border-2 border-gray-200'/>
+                                        <div className='flex gap-5 justify-between mt-4'>
+                                            <p className=' bg-blue-200 text-center rounded-md w-[30rem] px-4 py-3'>{item.subParameters[0].validate.options[0].label}</p>
+                                            <p className=' bg-blue-200 text-center rounded-md w-[30rem] px-4 py-3'>{item.subParameters[0].validate.options[1].label}</p>
                                         </div>
-                                    ) : index === 1 ? (
-                                        <div>
-                                            <h1>{item.label}</h1>
-                                            <div className='flex gap-5 justify-between'>
-                                                <button className=' bg-blue-200 p-5'>{item.subParameters[0].validate.options[0].label}</button>
-                                                <button className=' bg-blue-200 p-5'>{item.subParameters[0].validate.options[1].label}</button>
-                                            </div>
-                                            {/* Slices sub section  */}
+                                        {/* Slices sub section  */}
+                                        <div className='flex justify-between mt-5'>
+                                            <h1 className='font-bold pt-2'>{item.subParameters[1].subParameters[0].label}</h1>
+                                
+                                            <select className='w-[30rem] h-[3rem] rounded-md outline-none bg-blue-100 p-2'>
+                                                {item.subParameters[1].subParameters[0].validate.options.map((slice) => (
+                                                    <option value={slice.value}>{slice.value}</option>
+                                                ))}
+                                            </select>
+
+
+
+                                        </div>
+                                    </div>
+
+                                ) : index === 2 ? (
+                                    <div>
+                                        {/* Toppings Section  */}
+                                        <div >
+                                            <h1 className=' font-bold'>{item.label}</h1>
+                                             <hr className='my-4 border-2 border-gray-200'/>
+                                            {/* Sauce sub-section  */}
                                             <div className='flex justify-between mt-5'>
-                                                <h1>{item.subParameters[1].subParameters[0].label}</h1>
-                                                {/* <button className='bg-blue-200'>dropdown</button> */}
-                                                <select className='w-[30rem] h-[3rem] rounded-md'>
-                                                    {item.subParameters[1].subParameters[0].validate.options.map((slice) => (
+                                                <h1  className=' font-bold'>{item.subParameters[0].label}</h1>
+                                                {/* DropDown  */}
+                                                <select className='w-[30rem] h-[3rem] rounded-md outline-none bg-blue-100 p-2'>
+                                                    {item.subParameters[0].validate.options.map((slice) => (
                                                         <option value={slice.value}>{slice.value}</option>
                                                     ))}
                                                 </select>
-
-
-
                                             </div>
-                                        </div>
+                                            {/* Main Topping sub-section  */}
+                                            <div className='flex justify-between mt-5'>
+                                                <h1  className=' font-bold'>{item.subParameters[1].label}</h1>
+                                                {/* DropDown  */}
+                                                <select className='w-[30rem] h-[3rem] rounded-md outline-none bg-blue-100 p-2'>
+                                                    {item.subParameters[1].validate.options.map((slice) => (
+                                                        <option value={slice.value}>{slice.value}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
 
-                                    ) : index === 2 ? (
-                                        <div>
-                                            {/* Toppings Section  */}
-                                            <div >
-                                                <h1>{item.label}</h1>
-                                                {/* Sauce sub-section  */}
-                                                <div className='flex justify-between mt-5'>
-                                                    <h1>{item.subParameters[0].label}</h1>
-                                                    {/* DropDown  */}
-                                                    <select className='w-[30rem] h-[3rem] rounded-md'>
-                                                        {item.subParameters[0].validate.options.map((slice) => (
-                                                            <option value={slice.value}>{slice.value}</option>
-                                                        ))}
-                                                    </select>
-                                                </div>
-                                                {/* Main Topping sub-section  */}
-                                                <div className='flex justify-between mt-5'>
-                                                    <h1>{item.subParameters[1].label}</h1>
-                                                    {/* DropDown  */}
-                                                    <select className='w-[30rem] h-[3rem] rounded-md'>
-                                                        {item.subParameters[1].validate.options.map((slice) => (
-                                                            <option value={slice.value}>{slice.value}</option>
-                                                        ))}
-                                                    </select>
-                                                </div>
+                                            {/*Include seasoning checkbox*/}
 
-                                                {/*Include seasoning checkbox*/}
+                                            <div className='flex gap-5'>
+                                                <input type="checkbox" className='bg-red-500 border-blue-500' name='' id="" />
+                                                <h1 className='pt-2 font-bold'>{item.subParameters[2].label}</h1>
+                                            </div>
 
-                                                <div className='flex'>
-                                                    <input type="checkbox" name='' id="" />
-                                                    <h1>{item.subParameters[2].label}</h1>
-                                                </div>
-
-                                                <div className='mt-5'>
-                                                    {toggle ? (
-                                                        <div className='flex justify-between'>
-                                                            <h1>{item.subParameters[3].label}</h1>
-                                                            <select className='w-[30rem] h-[3rem] rounded-md'>
-                                                                {item.subParameters[3].validate.options.map((slice) => (
-                                                                    <option value={slice.value}>{slice.value}</option>
-                                                                ))}
-                                                            </select>
-                                                        </div>
-
-                                                    ) : null}
-                                                    <div className='flex gap-4'>
-                                                        {toggle ? (
-                                                            <h1>Hide advanced field</h1>
-                                                        ) : (
-                                                            <h1>Show advanced field</h1>
-                                                        )}
-                                                        <Switch size='small' onChange={onChange} />
+                                            <div className='mt-5'>
+                                                {toggle ? (
+                                                    <div className='flex justify-between'>
+                                                        <h1  className=' font-bold'>{item.subParameters[3].label}</h1>
+                                                        <select className='p-2 w-[30rem] h-[3rem] rounded-md outline-none bg-blue-100'>
+                                                            {item.subParameters[3].validate.options.map((slice) => (
+                                                                <option value={slice.value}>{slice.value}</option>
+                                                            ))}
+                                                        </select>
                                                     </div>
 
-
-                                                </div>
-
-                                            </div>
-
-                                        </div>
-
-                                    ) : index === 3 ? (
-
-                                        <div>
-                                            {toggleSS ? (
-                                                <div className='flex justify-between'>
-                                                    <h1>{item.label}</h1>
-                                                    <select className='w-[30rem] h-[3rem] rounded-md'>
-                                                        {item.validate.options.map((slice) => (
-                                                            <option value={slice.value}>{slice.value}</option>
-                                                        ))}
-                                                    </select>
-                                                </div>
-
-                                            ) : null}
-                                            <div className='flex gap-4 justify-between'>
+                                                ) : null}
                                                 <div className='flex gap-4'>
-                                                    {toggleSS ? (
-                                                        <h1>Hide advanced field</h1>
+                                                    {toggle ? (
+                                                        <h1  className=' font-bold'>Hide advanced field</h1>
                                                     ) : (
-                                                        <h1>Show advanced field</h1>
+                                                        <h1  className=' font-bold'>Show advanced field</h1>
                                                     )}
-                                                    <Switch size='small' onChange={onChangeAnother} />
+                                                    <Switch size='small' className='bg-gray-300' onChange={onChange} />
                                                 </div>
 
-                                                <div className='flex gap-5'>
-                                                    <button className=' bg-transparent border-red-300'>Cancel</button>
-                                                    <button type='submit' className=' bg-gray-400 text-white'>Submit</button>
 
-                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                ) : index === 3 ? (
+
+                                    <div>
+                                        {toggleSS ? (
+                                            <div className='flex justify-between'>
+                                                <h1  className=' font-bold'>{item.label}</h1>
+                                                <select className='p-2 w-[30rem] h-[3rem] rounded-md outline-none bg-blue-100'>
+                                                    {item.validate.options.map((slice) => (
+                                                        <option value={slice.value}>{slice.value}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
+                                             
+
+                                        ) : null}
+                                        <div className='flex gap-4 justify-between mt-5'>
+                                            <div className='flex gap-4  '>
+                                      
+                                                {toggleSS ? (
+                                                    <h1  className=' font-bold'>Hide advanced field</h1>
+                                                ) : (
+                                                    <h1  className=' font-bold'>Show advanced field</h1>
+                                                )}
+                                                <Switch size='small' className='bg-gray-300' onChange={onChangeAnother} />
+                                            </div>
+
+                                            <div className='flex gap-5'>
+                                                <button className=' bg-red-300 px-4 py-3 rounded-md'>Cancel</button>
+                                                <button type='submit' className=' bg-gray-400 px-4 py-3 rounded-md text-white'>Submit</button>
+
                                             </div>
                                         </div>
-                                    ) : null}
-                                </div>
+                                    </div>
+                                ) : null}
+                            </div>
 
-                          
+
 
                         </form>
                     )
